766063a832274eeeb77d8e4ee766eaed5550aa04
abdfnx <abdfnabd@gmail.com>
2021-09-05 19:14:13 +0300

    generate auth checker

---
+++ core/renderer/router/auth-check.js
@@ -0,0 +1,16 @@
+export default (to, from, next) => {
+  const isAuthPage = to.matched.some((record) => record.meta.auth);
+
+  const access_token = localStorage.getItem("access_token");
+  if (access_token) {
+    if (isAuthPage) {
+      return next({ name: "Home" });
+    }
+  } else {
+    if (!isAuthPage) {
+      return next({ name: "Login" });
+    }
+  }
+
+  next();
+};
