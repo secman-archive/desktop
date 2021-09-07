45ff8635e306b61455cb6c86100bba8787a4b5c0
abdfnx <abdfnabd@gmail.com>
2021-09-07 11:25:32 +0300

    add main entry file, fix routers issue

--- core/renderer/router/index.js
+++ core/renderer/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-
 import CheckAuth from "./auth-check";
 
 Vue.use(Router);
@@ -38,73 +37,73 @@ const router = new Router({
             },
           ],
         },
-      ],
-    },
-    {
-      path: "/credit-cards",
-      name: "CreditCards",
-      component: require("@/contents/CreditCards/index").default,
-      children: [
         {
-          path: "create",
-          name: "CreditCardCreate",
-          component: require("@/contents/CreditCards/Create").default,
+          path: "/credit-cards",
+          name: "CreditCards",
+          component: require("@/contents/CreditCards/index").default,
+          children: [
+            {
+              path: "create",
+              name: "CreditCardCreate",
+              component: require("@/contents/CreditCards/Create").default,
+            },
+            {
+              path: ":id",
+              name: "CreditCardDetail",
+              component: require("@/contents/CreditCards/Detail").default,
+            },
+          ],
         },
         {
-          path: ":id",
-          name: "CreditCardDetail",
-          component: require("@/contents/CreditCards/Detail").default,
-        },
-      ],
-    },
-    {
-      path: "/emails",
-      name: "Emails",
-      component: require("@/contents/Emails/index").default,
-      children: [
-        {
-          path: "create",
-          name: "EmailCreate",
-          component: require("@/contents/Emails/Create").default,
-        },
-        {
-          path: ":id",
-          name: "EmailDetail",
-          component: require("@/contents/Emails/Detail").default,
-        },
-      ],
-    },
-    {
-      path: "/notes",
-      name: "Notes",
-      component: require("@/contents/Notes/index").default,
-      children: [
-        {
-          path: "create",
-          name: "NoteCreate",
-          component: require("@/contents/Notes/Create").default,
+          path: "/emails",
+          name: "Emails",
+          component: require("@/contents/Emails/index").default,
+          children: [
+            {
+              path: "create",
+              name: "EmailCreate",
+              component: require("@/contents/Emails/Create").default,
+            },
+            {
+              path: ":id",
+              name: "EmailDetail",
+              component: require("@/contents/Emails/Detail").default,
+            },
+          ],
         },
         {
-          path: ":id",
-          name: "NoteDetail",
-          component: require("@/contents/Notes/Detail").default,
+          path: "/notes",
+          name: "Notes",
+          component: require("@/contents/Notes/index").default,
+          children: [
+            {
+              path: "create",
+              name: "NoteCreate",
+              component: require("@/contents/Notes/Create").default,
+            },
+            {
+              path: ":id",
+              name: "NoteDetail",
+              component: require("@/contents/Notes/Detail").default,
+            },
+          ],
         },
-      ],
-    },
-    {
-      path: "/servers",
-      name: "Servers",
-      component: require("@/contents/Servers/index").default,
-      children: [
         {
-          path: "create",
-          name: "ServerCreate",
-          component: require("@/contents/Servers/Create").default,
-        },
-        {
-          path: ":id",
-          name: "ServerDetail",
-          component: require("@/contents/Servers/Detail").default,
+          path: "/servers",
+          name: "Servers",
+          component: require("@/contents/Servers/index").default,
+          children: [
+            {
+              path: "create",
+              name: "ServerCreate",
+              component: require("@/contents/Servers/Create").default,
+            },
+            {
+              path: ":id",
+              name: "ServerDetail",
+              component: require("@/contents/Servers/Detail").default,
+            },
+          ],
         },
       ],
     },
