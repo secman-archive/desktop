2922b27dd9c3024b3db575a8cc6c94c5845c8d41
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-09-30 12:14:29 +0300

    fix secman desktop update checker

--- core/renderer/contents/Home/Sidebar/index.vue
+++ core/renderer/contents/Home/Sidebar/index.vue
@@ -1,238 +1,237 @@
 <template>
-	<div class="sidebar">
-		<div class="account">
-			<!-- Avatar -->
-			<VAvatar :name="user.name" />
-			<!-- Info -->
-			<div class="account-info">
-				<span class="account-info-name" v-text="user.name" />
-			</div>
+  <div class="sidebar">
+    <div class="account">
+      <!-- Avatar -->
+      <VAvatar :name="user.name" />
+      <!-- Info -->
+      <div class="account-info">
+        <span class="account-info-name" v-text="user.name" />
+      </div>
 
-			<!-- Menu -->
-			<button @click="showAccountMenu = !showAccountMenu">
-				<VIcon
-					:name="showAccountMenu ? 'x' : 'menu'"
-					size="15px"
-					class="c-white"
-				/>
-			</button>
+      <!-- Menu -->
+      <button @click="showAccountMenu = !showAccountMenu">
+        <VIcon
+          :name="showAccountMenu ? 'x' : 'menu'"
+          size="15px"
+          class="c-white"
+        />
+      </button>
 
-			<!-- Menu -->
-			<div class="account-menu" :class="accountMenuClass">
-				<div class="d-flex flex-column flex-items-start p-3">
-					<!-- Logout -->
-					<button class="c-danger mr-3" @click="onClickLogout">
-						{{ $t("Logout") }}
-						<VIcon name="logout" size="14px" rotation="180" class="ml-2" />
-					</button>
+      <!-- Menu -->
+      <div class="account-menu" :class="accountMenuClass">
+        <div class="d-flex flex-column flex-items-start p-3">
+          <!-- Logout -->
+          <button class="c-danger mr-3" @click="onClickLogout">
+            {{ $t("Logout") }}
+            <VIcon name="logout" size="14px" rotation="180" class="ml-2" />
+          </button>
 
-					<button class="mr-3" @click="onClickFeedback">
-						{{ $t("GiveFeedback") }}
-						<VIcon name="external-link" size="14px" class="ml-2" />
-					</button>
+          <button class="mr-3" @click="onClickFeedback">
+            {{ $t("GiveFeedback") }}
+            <VIcon name="external-link" size="14px" class="ml-2" />
+          </button>
 
-					<div class="version">
-						<p class="pt-3">
-							Version:<strong>&nbsp;{{ _version() }}</strong>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
+          <div class="version">
+            <p class="pt-3">
+              Version:<strong>&nbsp;{{ _version() }}</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-		<!-- Logins -->
-		<MenuItem
-			:service="$C.Services.Logins"
-			:name="$t('Logins')"
-			icon="lock-closed"
-		/>
+    <!-- Logins -->
+    <MenuItem
+      :service="$C.Services.Logins"
+      :name="$t('Logins')"
+      icon="lock-closed"
+    />
 
-		<!-- Credit Cards -->
-		<MenuItem
-			:service="$C.Services.CreditCards"
-			:name="$t('Credit Cards')"
-			icon="credit-card"
-		/>
+    <!-- Credit Cards -->
+    <MenuItem
+      :service="$C.Services.CreditCards"
+      :name="$t('Credit Cards')"
+      icon="credit-card"
+    />
 
-		<!-- Emails -->
-		<MenuItem :service="$C.Services.Emails" :name="$t('Emails')" icon="email" />
+    <!-- Emails -->
+    <MenuItem :service="$C.Services.Emails" :name="$t('Emails')" icon="email" />
 
-		<!-- Private Notes -->
-		<MenuItem
-			:service="$C.Services.Notes"
-			:name="$t('Private Notes')"
-			icon="private-note"
-		/>
+    <!-- Private Notes -->
+    <MenuItem
+      :service="$C.Services.Notes"
+      :name="$t('Private Notes')"
+      icon="private-note"
+    />
 
-		<!-- Servers -->
-		<MenuItem
-			:service="$C.Services.Servers"
-			:name="$t('Servers')"
-			icon="server"
-		/>
+    <!-- Servers -->
+    <MenuItem
+      :service="$C.Services.Servers"
+      :name="$t('Servers')"
+      icon="server"
+    />
 
-		<button class="btn-empty-fix"></button>
+    <button class="btn-empty-fix"></button>
 
-		<!-- Update -->
-		<button
-			v-if="hasUpdate"
-			@click="onClickUpdateApp"
-			class="update-box flex-center"
-		>
-			{{ $t("There is an update available.") }}
-		</button>
+    <!-- Update -->
+    <button
+      v-if="hasUpdate"
+      @click="onClickUpdateApp"
+      class="update-box flex-center"
+    >
+      {{ $t("There is an update available.") }}
+    </button>
 
-		<!-- Support -->
-		<button class="btn-feedback" @click="onClickSupport">
-			<div class="icon">
-				<VIcon v-tooltip="'Get Support'" name="support" size="14px" />
-			</div>
-		</button>
-	</div>
+    <!-- Support -->
+    <button class="btn-feedback" @click="onClickSupport">
+      <div class="icon">
+        <VIcon v-tooltip="'Get Support'" name="support" size="14px" />
+      </div>
+    </button>
+  </div>
 </template>
 
 <script>
-import HTTPClient from "@/apis/http";
 import MenuItem from "./MenuItem";
 import electron from "electron";
-import { mapActions, mapState } from "vuex";
+import { mapGetters, mapState } from "vuex";
+import Axios from "axios";
 
 export default {
-	components: {
-		MenuItem
-	},
+  components: {
+    MenuItem,
+  },
 
-	data() {
-		return {
-			hasUpdate: false,
-			updateLink: null,
-			showAccountMenu: false
-		};
-	},
+  data() {
+    return {
+      hasUpdate: false,
+      updateLink: null,
+      showAccountMenu: false,
+    };
+  },
 
-	created() {
-		this.checkUpdate();
-	},
+  created() {
+    this.checkUpdate();
+  },
 
-	computed: {
-		...mapState(["user"]),
+  computed: {
+    ...mapState(["user"]),
 
-		accountMenuClass() {
-			return { "--open": this.showAccountMenu };
-		}
-	},
+    accountMenuClass() {
+      return { "--open": this.showAccountMenu };
+    },
+  },
 
-	methods: {
-		...mapActions(["Logout"]),
+  methods: {
+    ...mapActions(["Logout"]),
 
-		onClickUpdateApp() {
-			electron.shell.openExternal("https://d.secman.dev");
-		},
+    onClickUpdateApp() {
+      electron.shell.openExternal("https://d.secman.dev");
+    },
 
-		async checkUpdate() {
-			const { version } = require("../../../../../package.json");
-			try {
-				const { data } = await HTTPClient.get(
-					"https://api.github.com/repos/scmn-dev/desktop/releases/latest",
-					{},
-					{ Authorization: null }
-				);
+    async checkUpdate() {
+      const { version } = require("../../../../../package.json");
+      try {
+        const { data } = await Axios.get(
+          "https://api.github.com/repos/scmn-dev/desktop/releases/latest",
+          {}
+        );
 
-				this.hasUpdate = data.tag_name != "v" + version;
-				// this.updateLink = data.html_url;
-			} catch (err) {
-				console.log(err);
-			}
-		},
+        this.hasUpdate = data.tag_name != "v" + version;
+        this.updateLink = data.html_url;
+      } catch (err) {
+        console.log(err);
+      }
+    },
 
-		onClickFeedback() {
-			// electron.shell.openExternal("https://secman.typeform.com"); when secman typeform is ready
-		},
+    onClickFeedback() {
+      // electron.shell.openExternal("https://secman.typeform.com"); when secman typeform is ready
+    },
 
-		onClickSupport() {
-			electron.shell.openExternal("https://secman.dev/support");
-		},
+    onClickSupport() {
+      electron.shell.openExternal("https://secman.dev/support");
+    },
 
-		onClickUpdate() {
-			electron.shell.openExternal(this.user.update_url);
-		},
+    onClickUpdate() {
+      electron.shell.openExternal(this.user.update_url);
+    },
 
-		onClickCancel() {
-			electron.shell.openExternal(this.user.cancel_url);
-		},
+    onClickCancel() {
+      electron.shell.openExternal(this.user.cancel_url);
+    },
 
-		onClickLogout() {
-			this.Logout();
-			this.$router.replace({ name: "Login" });
-		},
+    onClickLogout() {
+      this.Logout();
+      this.$router.replace({ name: "Login" });
+    },
 
-		_version() {
-			const { version } = require("../../../../../package.json");
-			return "v" + version;
-		},
+    _version() {
+      const { version } = require("../../../../../package.json");
+      return "v" + version;
+    },
 
-		async onExport() {
-			const filePath = remote.dialog.showSaveDialogSync(null);
+    async onExport() {
+      const filePath = remote.dialog.showSaveDialogSync(null);
 
-			if (!filePath) {
-				return;
-			}
+      if (!filePath) {
+        return;
+      }
 
-			try {
-				const data = await this.Export();
+      try {
+        const data = await this.Export();
 
-				const itemList = JSON.parse(CryptoUtils.aesDecrypt(data));
-				itemList.forEach(item => CryptoUtils.decryptFields(item));
+        const itemList = JSON.parse(CryptoUtils.aesDecrypt(data));
+        itemList.forEach((item) => CryptoUtils.decryptFields(item));
 
-				const csvContent = Papa.unparse(itemList);
-				fs.writeFileSync(filePath, csvContent);
-			} catch (error) {
-				this.$notifyError(this.$t("Something went wrong."));
-				console.log(error);
-			}
-		},
+        const csvContent = Papa.unparse(itemList);
+        fs.writeFileSync(filePath, csvContent);
+      } catch (error) {
+        this.$notifyError(this.$t("Something went wrong."));
+        console.log(error);
+      }
+    },
 
-		onImport() {
-			remote.dialog
-				.showOpenDialog({ properties: ["openFile"] })
-				.then(async ({ filePaths }) => {
-					if (filePaths.length === 0) {
-						return;
-					}
-					try {
-						const fileContent = fs.readFileSync(filePaths[0]).toString();
+    onImport() {
+      remote.dialog
+        .showOpenDialog({ properties: ["openFile"] })
+        .then(async ({ filePaths }) => {
+          if (filePaths.length === 0) {
+            return;
+          }
+          try {
+            const fileContent = fs.readFileSync(filePaths[0]).toString();
 
-						let parsedCSV = Papa.parse(fileContent.trim(), {
-							header: true // creates array of { head: value }
-						});
+            let parsedCSV = Papa.parse(fileContent.trim(), {
+              header: true, // creates array of { head: value }
+            });
 
-						if (parsedCSV.errors.length > 0) {
-							this.$notifyError(
-								this.$t(
-									"There is an error. Error: ",
-									parsedCSV.errors[0].message
-								)
-							);
-							return;
-						}
+            if (parsedCSV.errors.length > 0) {
+              this.$notifyError(
+                this.$t(
+                  "There is an error. Error: ",
+                  parsedCSV.errors[0].message
+                )
+              );
+              return;
+            }
 
-						const itemList = parsedCSV.data.map(item => {
-							return CryptoUtils.encryptPayload(item, [
-								"username",
-								"password",
-								"extra"
-							]);
-						});
+            const itemList = parsedCSV.data.map((item) => {
+              return CryptoUtils.encryptPayload(item, [
+                "username",
+                "password",
+                "extra",
+              ]);
+            });
 
-						await this.Import(itemList);
-						this.FetchAll();
-					} catch (error) {
-						this.$notifyError(this.$t("Something went wrong."));
-						console.log(error);
-					}
-				});
-		}
-	}
+            await this.Import(itemList);
+            this.FetchAll();
+          } catch (error) {
+            this.$notifyError(this.$t("Something went wrong."));
+            console.log(error);
+          }
+        });
+    },
+  },
 };
 </script>
 
